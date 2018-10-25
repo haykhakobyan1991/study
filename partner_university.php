88947e1e3dd235969f491d3ea400c0986f513141
haykhakobyan1991 <haikhakobyan2@gmail.com>
2018-10-25 23:40:25 +0400

    Hayk

--- partner_university.php
+++ partner_university.php
@@ -52,7 +52,71 @@
 <div class="container">
     <div class="center" style="overflow: hidden;">
 
+        <div class="center container_cube" style="overflow: hidden;">
+            <h2 class="about_us">Partner University</h2>
+        </div>
 
+        <div class="content" style="text-align: center;">
+            Select a university to find out more.
+        </div>
+
+        <div  style="width: 90%;text-align: center;margin: 0 auto;">
+            <div class="row">
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+                <div class="content container_cube" style="text-align: center;">
+                    <a href="#">Aberdeen</a>
+                </div>
+            </div>
+
+        </div>
 
     </div>
 </div>
