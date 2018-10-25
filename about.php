99b5aec17e70f0d0d9a5319b51b81f3c7e109b3a
haykhakobyan1991 <haikhakobyan2@gmail.com>
2018-10-25 23:19:02 +0400

    Hayk about.php

--- about.php
+++ about.php
@@ -50,11 +50,59 @@
 
 
 <div class="container">
+    <div class="center" style="overflow: hidden;">
+        <h2 class="about_us">About us</h2>
 
+        <div class="content">
+            Global Study UK are a London based educational consultancy specialising in student recruitment and support
+            with local offices in Cairo, Alexandria, Beirut, Jbeil and Muscat. Our London office is available for
+            students to visit prior to enrollment and during their studies. We have over 50 years of education
+            experience and pride ourselves on our commitment to student success and academic progression. We offer free
+            assistance from the first to the last steps of your application, ensuring your receive an offer for a
+            foundation or degree programme at a British university on the course of your choice.
+        </div>
+
+        <h2 class="about_us">Why apply with us?</h2>
+
+        <div class="content">
+            <p>. Continuous support throughout your education</p>
+
+            <p>. Our vision is to ensure all students receive an offer</p>
+
+            <p>. Regional offices in the MENA region</p>
+
+            <p>. We are an official UCAS application centre</p>
+
+            <p>. Support with Tier 4 UK visa applications</p>
+
+            <p>. Support with finding university accommodation</p>
+
+
+            <p>. All advice and guidance is free</p>
+        </div>
+        <br>
+
+        <div class="content">
+            <p>. End-to-end assistance achieving recruitment goals</p>
+
+            <p>. Exhibitions across the Middle East and North Africa</p>
+
+            <p>. Special relationships with schools and colleges abroad</p>
+
+            <p>. We pursue student inquiries from our events</p>
+
+            <p>. We attract capable and highly educated students</p>
+
+            <p>. Vast marketing power to over 1.5m social media fans</p>
+
+            <p>. We are experiencing rapid year-on-year growth</p>
+        </div>
+
+    </div>
 </div>
 
 <footer>
-    <div class="center" style="padding-top: 50px;" >
+    <div class="center" style="padding-top: 50px;">
         <ul>
             <li>example@example.com</li>
             <li>+(789) 456 456</li>
